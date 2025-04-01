133b0fbec015ace91583e24cb30bf8ff608ea7ae
rajashri1409 <2023bit003@sggs.ac.in>
2025-04-01 23:27:45 +0530

    Add files via upload

---
+++ Practical12/Fun.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author RAJASHRI BHOSALE
+ */
+public class Fun {
+    String name;
+    int age;
+
+    public Fun() {
+        this.age =age;
+        
+                
+    }
+    
+}
